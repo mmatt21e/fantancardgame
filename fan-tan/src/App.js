06dd6aa99bac28383f902f282e873dad1bccf34e
Matt <wsop1978@gmail.com>
2022-02-17 10:01:16 -0500

    Start

---
+++ fan-tan/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Header,Layout, RedRectangle} from './Layout.components';
+
+function App() {
+  return (
+    <div className="App">
+      <Header>Fan-Tan</Header>
+      <RedRectangle />
+    </div>
+  );
+}
+
+export default App;
---
+++ fan-tan/src/cardGameBoard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+class cardGameBoard extends React.Component{
+    state = {
+
+
+
+    }
+
+    componentDidMount = () => {
+
+
+    }
+
+    render(){
+
+        return(
+            <div>Something</div>
+        )
+    }
+    
+}
+export default cardGameBoard;
